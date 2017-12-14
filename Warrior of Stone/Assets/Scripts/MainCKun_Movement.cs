95ff9816d99d957ca1e0d86d6c2cf369e0cc22ac
DALEK-PC\Eleanor <eap9238@g.rit.edu>
2017-12-14 18:23:19 -0500

    Successfully added the WOS Unity game

---
+++ Warrior of Stone/Assets/Scripts/MainCKun_Movement.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCKun_Movement : MonoBehaviour {
+
+	private Rigidbody2D mCharEngine;
+	private Vector3 tempVelocity;
+
+	public float maxXVelocity;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mCharEngine = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		tempVelocity = mCharEngine.velocity;
+
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			//print("up arrow key is held down");
+
+			//This will be for climbing ladders and stuff
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			//print("down arrow key is held down");
+
+			//this will be or ducking and ladders and stuff
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			//print("left arrow key is held down");
+
+			tempVelocity.x -= 1;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			//print("right arrow key is held down");
+
+			tempVelocity.x += 1;
+		}
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			//print("w key is held down");
+
+			//This will be for climbing ladders and stuff
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			//print("s key is held down");
+
+			//this will be or ducking and ladders and stuff
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			//print("a key is held down");
+
+			tempVelocity.x -= 1;
+		}
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			//print("d key is held down");
+
+			tempVelocity.x += 1;
+		}
+
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			//print("space bar is held down");
+
+			tempVelocity.y += 1;
+		}
+
+		if (tempVelocity.x > maxXVelocity) 
+		{
+			tempVelocity.x = maxXVelocity;
+		} 
+		else if (tempVelocity.x < (-1) * maxXVelocity) 
+		{
+			tempVelocity.x = (-1) * maxXVelocity;
+		}  
+
+		mCharEngine.velocity = tempVelocity;
+	}
+}
